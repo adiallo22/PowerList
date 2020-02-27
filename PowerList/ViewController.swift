19eda8170a957acd09a06f4cc501b52865e72f27
abdul diallo <rahimi.diallo96@gmail.com>
2020-02-27 08:09:52 -0500

    install Realm to persist data

--- PowerList/ViewController.swift
+++ PowerList/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableOfitem: UITableView!
+    @IBOutlet weak var completedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
+
+//MARK: - <#section heading#>
+
 
